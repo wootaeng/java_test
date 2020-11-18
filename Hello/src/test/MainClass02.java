1380c88482f2724c2076d07646d8eb2f00d9b356
karu <karufuci@gmail.com>
2020-11-18 16:00:48 +0900

    hello add

---
+++ Hello/src/test/MainClass02.java
@@ -0,0 +1,13 @@
+package test;
+
+public class MainClass02 {
+	//run 했을때 실행의 흐름이 시작되는 특별한 main 메소드
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("하나");
+		System.out.println("두울");
+		System.out.println("세엣");
+		
+	}
+
+}
